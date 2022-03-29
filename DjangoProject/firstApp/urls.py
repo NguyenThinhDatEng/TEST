c786aaa0a197e8062f6d354074c80985419795ee
NguyenThinhDatEng <thinh.nv194178@sis.hust.edu.vn>
2022-03-29 19:49:09 +0700

    static - app_name

--- DjangoProject/firstApp/urls.py
+++ DjangoProject/firstApp/urls.py
@@ -2,6 +2,11 @@ from secrets import choice
 from django.urls import path
 from . import views
 
+# because many applications have the same view names
+# {% url 'app:view' %}
+
+app_name = 'firstApp'
+
 urlpatterns = [
     path('', views.hello, name='home'),
     path('questions/', views.listOfQuestions, name='list'),
