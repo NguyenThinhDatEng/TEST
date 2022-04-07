8a268ce7ca1c08c3be7f2883713fd70a8e017d39
NguyenThinhDatEng <toithienamxinnghe@gmail.com>
2022-04-07 09:42:17 +0700

    customize user model

--- DjangoProject/firstApp/models.py
+++ DjangoProject/firstApp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
@@ -16,3 +17,12 @@ class Choice(models.Model):
     # when the question is removed, the choice also is deleted
     answer = models.TextField(max_length=100, null=False)
     choices = models.IntegerField(default=0)
+
+# customize user in django
+
+
+class User(AbstractUser):
+    sex_choices = ((0, 'Male'), (1, 'Female'), (2, 'Unknown'))
+    phone_number = models.CharField(max_length=11, null=False)
+    # choices = object
+    sex = models.IntegerField(choices=sex_choices, default=0)
